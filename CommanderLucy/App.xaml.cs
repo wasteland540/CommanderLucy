cf0d3eb26b894f5be8c30ebbe7c69dc3eb224821
wasteland540 <m_m_elz@web.de>
2016-04-06 16:27:41 +0200

    Basic Sliding Panel added.

--- CommanderLucy/App.xaml.cs
+++ CommanderLucy/App.xaml.cs
@@ -8,7 +8,7 @@ namespace CommanderLucy
     /// <summary>
     ///     Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
         public IUnityContainer Container;
 
--- CommanderLucy/Views/MainWindow.xaml.cs
+++ CommanderLucy/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using CommanderLucy.ViewModels;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using CommanderLucy.ViewModels;
 using Microsoft.Practices.Unity;
 
 namespace CommanderLucy.Views
@@ -17,6 +20,47 @@ namespace CommanderLucy.Views
         public MainWindowViewModel ViewModel
         {
             set { DataContext = value; }
+        }
+
+        private void btnTopMenuHide_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbHideTopMenu1", btnTopMenuHide, btnTopMenuShow, pnlTopMenu);
+            btnTopMenuShow2.Visibility = Visibility.Visible;
+        }
+
+        private void btnTopMenuShow_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbShowTopMenu1", btnTopMenuHide, btnTopMenuShow, pnlTopMenu);
+            btnTopMenuShow2.Visibility = Visibility.Hidden;
+        }
+
+        private void btnTopMenu2Hide_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbHideTopMenu2", btnTopMenuHide2, btnTopMenuShow2, pnlTopMenu2);
+            btnTopMenuShow.Visibility = Visibility.Visible;
+        }
+
+        private void btnTopMenu2Show_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbShowTopMenu2", btnTopMenuHide2, btnTopMenuShow2, pnlTopMenu2);
+            btnTopMenuShow.Visibility = Visibility.Hidden;
+        }
+
+        private void ShowHideMenu(string Storyboard, Button btnHide, Button btnShow, StackPanel pnl)
+        {
+            var sb = Resources[Storyboard] as Storyboard;
+            sb.Begin(pnl);
+
+            if (Storyboard.Contains("Show"))
+            {
+                btnHide.Visibility = Visibility.Visible;
+                btnShow.Visibility = Visibility.Hidden;
+            }
+            else if (Storyboard.Contains("Hide"))
+            {
+                btnHide.Visibility = Visibility.Hidden;
+                btnShow.Visibility = Visibility.Visible;
+            }
         }
     }
 }
