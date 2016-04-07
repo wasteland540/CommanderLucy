f0a514e2d0e440fc5d48b6e1f88343758b886d43
wasteland540 <m_m_elz@web.de>
2016-04-07 21:49:04 +0200

    Plugin Manager View designed.

--- CommanderLucy/ViewModels/MainWindowViewModel.cs
+++ CommanderLucy/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,43 @@
-﻿using CommanderLucy.ViewModels.Base;
+﻿using System.Windows.Input;
+using CommanderLucy.Commands;
+using CommanderLucy.ViewModels.Base;
+using CommanderLucy.Views;
+using GalaSoft.MvvmLight.Messaging;
+using Microsoft.Practices.Unity;
 
 namespace CommanderLucy.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private readonly IMessenger _messenger;
+        private ICommand _openPluginManagerCommand;
+
+        public MainWindowViewModel(IMessenger messenger)
+        {
+            _messenger = messenger;
+        }
+
+        #region Properties
+
+        public ICommand OpenPluginManagerCommand
+        {
+            get
+            {
+                _openPluginManagerCommand = _openPluginManagerCommand ?? new DelegateCommand(OpenPluginManagerView);
+                return _openPluginManagerCommand;
+            }
+        }
+
+        #endregion Properties
+
+        #region Private Methods
+
+        private void OpenPluginManagerView(object obj)
+        {
+            var pluginManagerView = Container.Resolve<PluginView>();
+            pluginManagerView.ShowDialog();
+        }
+
+        #endregion Private Methods
     }
 }
--- CommanderLucy/ViewModels/PluginViewModel.cs
+++ CommanderLucy/ViewModels/PluginViewModel.cs
@@ -1,8 +1,33 @@
-﻿using CommanderLucy.ViewModels.Base;
+﻿using System.Windows.Input;
+using CommanderLucy.Commands;
+using CommanderLucy.ViewModels.Base;
 
 namespace CommanderLucy.ViewModels
 {
     public class PluginViewModel : ViewModelBase
     {
+        private ICommand _deletePluginCommand;
+
+        #region Properties
+
+        public ICommand DeletePluginCommand
+        {
+            get
+            {
+                _deletePluginCommand = _deletePluginCommand ?? new DelegateCommand(DeletePlugin);
+                return _deletePluginCommand;
+            }
+        }
+
+        #endregion Properties
+
+        #region Private Methods
+
+        private void DeletePlugin(object obj)
+        {
+            
+        }
+
+        #endregion Private Methods
     }
 }
